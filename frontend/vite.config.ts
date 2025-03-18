3bc19e891510d87687f983b0cb3ffea6f9f84f27
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2025-03-18 14:59:26 +0100

    Configure PWA Manifest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server : {
+  server: {
     proxy: {
       "/api": {
         target: 'http://localhost:3000/api/',
@@ -12,5 +13,36 @@ export default defineConfig({
       },
     }
   },
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    manifest: {
+      theme_color: "#556cd6",
+      description: "Découvrez les films diffusés à Rennes cette semaine!",
+      screenshots: [
+        {
+          src: "/mobile_screenshot_home.png",
+          form_factor: "narrow",
+          sizes: '1290x2796',
+          type: "image/png",
+          label: "Home Page (Mobile)"
+        },
+        {
+          src: "/mobile_screenshot_day.png",
+          form_factor: "narrow",
+          sizes: '1290x2796',
+          type: "image/png",
+          label: "Day by Day (Mobile)"
+        },
+        {
+          src: "/desktop_screenshot.png",
+          form_factor: "wide",
+          sizes: '3584x2240',
+          type: "image/png",
+          label: "Home Page (Desktop)"
+        }
+      ],
+    },
+    pwaAssets: {
+      image: "public/camera.svg"
+    }
+  })],
 })
