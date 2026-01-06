a8c8d0e3918720178801730a1da5f13b7aab3e06
deodat <deodat>
2026-01-06 21:58:38 +0100

    oops

--- frontend/src/HomePage/FilterSelector.tsx
+++ frontend/src/HomePage/FilterSelector.tsx
@@ -1,5 +1,6 @@
 import Box from '@mui/material/Box';
-import { Button, Paper } from '@mui/material';
+import { Button, FormControl, InputLabel, MenuItem, Paper, Select, SelectChangeEvent } from '@mui/material';
+import { useState } from 'react';
 
 export type FilterSelectorProps = {
     id: number,
@@ -38,13 +39,28 @@ export default function FilterSelector(props: FilterSelectorProps): JSX.Element 
     const options = ["Tous les films", "Sorties de la semaine", "Diffusions uniques", "Avant premières"];
     const opt_len = options.length;
 
-    function handleClick(id: number) {
-        props.setId(id)
-    }
+    const [filterText, setFilterText] = useState(options[0])
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setFilterText(event.target.value as string);
+    };
 
     return (
         <Box display={"flex"} flexDirection={"row"} flexWrap={"wrap"} width="lg" justifyContent={"center"}>
-            {Array.from(Array(opt_len).keys()).map((id: number) => (
+            <FormControl fullWidth>
+                <InputLabel>Filtre</InputLabel>
+                <Select
+                    defaultValue={0}
+                    value={filterText}
+                    label="Filtre"
+                    onChange={handleChange}
+                >
+                    {Array.from(Array(opt_len).keys()).map((id: number) => (
+                        <MenuItem value={id}>{options[id]}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            {/* {Array.from(Array(opt_len).keys()).map((id: number) => (
                 <Paper sx={get_button_style(props.id, id)} key={id}>
                     <Button sx={{
                         color: "text.primary"
@@ -54,7 +70,7 @@ export default function FilterSelector(props: FilterSelectorProps): JSX.Element 
                     </Button>
                 </Paper>
             ))
-            }
+            } */}
         </Box >
     )
 
